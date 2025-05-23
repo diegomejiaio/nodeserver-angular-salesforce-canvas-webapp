83d3235cc50bc47b9d17914e1485053833385270
Diego <diegomejiamagallanes7@gmail.com>
2025-05-23 12:07:58 -0500

    fixing styles

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { 
-  NglModule, 
+import {
+  NglModule,
   NglIconsModule,
   NglButtonsModule,
+  NglBadgesModule,
+  NglSpinnersModule,
+  NglToastModule,
 } from 'ng-lightning';
 
 /**
@@ -18,7 +21,16 @@ import {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, NglModule, NglIconsModule, NglButtonsModule],
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    NglModule, 
+    NglIconsModule, 
+    NglButtonsModule,
+    NglBadgesModule,
+    NglSpinnersModule,
+    NglToastModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -30,7 +42,7 @@ export class AppComponent implements OnInit {
   /** Controls the visibility of the top toast from ng-lightning */
   showTopToast = false;
   /** API endpoint for testing */
-  apiEndpoint = '/services/data/v52.0/sobjects/Opportunity/';
+  apiEndpoint = '/services/data/v58.0/sobjects/';
   /** API response */
   apiResponse: any = null;
   /** Loading state for API call */
@@ -44,7 +56,8 @@ export class AppComponent implements OnInit {
    */
   onClose(reason: string) {
     console.log(`Closed by ${reason}`);
-  };
+    this.showTopToast = false;
+  }
 
   ngOnInit() {
     // Check for globally injected Salesforce Canvas data
@@ -56,9 +69,101 @@ export class AppComponent implements OnInit {
     } else {
       console.log('No Salesforce Canvas data found, loading test data');
       // Load test data for development
-      alert('No Salesforce Canvas data found, loading test data');
+      
       // Simulate a signed request payload
-      this.envelope = { "algorithm": "HMACSHA256", "issuedAt": -246115464, "userId": "005gL00000dasdas232iC2jQAE", "client": { "refreshToken": null, "instanceId": "_:pep_web_app_2:j_id0:j_id1:canvasapp", "targetOrigin": "https://orgfarm-1ee7d40ed5-dev-ed.develop.my.salesforce.com", "instanceUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.my.salesforce.com", "oauthToken": "dasdasdasdassoyuntokenadsasdasda" }, "context": { "user": { "userId": "005gL000002iC2jQAE", "userName": "dadsasda04@agentforce.com", "firstName": "adsdasdasdas", "lastName": "Mejia", "asdasdasda": "adsasdasdasdas@adasdasdasd.com", "fullName": "asdasda asdasdasd", "locale": "es_PE", "language": "en_US", "timeZone": "America/Lima", "profileId": "00egL000001efNI", "roleId": null, "userType": "STANDARD", "currencyISOCode": "USD", "profilePhotoUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.file.force.com/profilephoto/005/F", "profileThumbnailUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.file.force.com/profilephoto/005/T", "siteUrl": null, "siteUrlPrefix": null, "networkId": null, "accessibilityModeEnabled": false, "isDefaultNetwork": true }, "links": { "loginUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.my.salesforce.com", "enterpriseUrl": "/services/Soap/c/63.0/00DgL000003KIqH", "metadataUrl": "/services/Soap/m/63.0/00DgL000003KIqH", "partnerUrl": "/services/Soap/u/63.0/00DgL000003KIqH", "restUrl": "/services/data/v63.0/", "sobjectUrl": "/services/data/v63.0/sobjects/", "searchUrl": "/services/data/v63.0/search/", "queryUrl": "/services/data/v63.0/query/", "recentItemsUrl": "/services/data/v63.0/recent/", "chatterFeedsUrl": "/services/data/v31.0/chatter/feeds", "chatterGroupsUrl": "/services/data/v63.0/chatter/groups", "chatterUsersUrl": "/services/data/v63.0/chatter/users", "chatterFeedItemsUrl": "/services/data/v31.0/chatter/feed-items", "userUrl": "/005gL000002iC2jQAE" }, "application": { "name": "pep_web_app_2", "canvasUrl": "https://sfcanvaswebapp002.azurewebsites.net", "applicationId": "06PgL0000004hCr", "version": "1.0", "authType": "SIGNED_REQUEST", "referenceId": "09HgL0000004D3V", "options": [], "samlInitiationMethod": "None", "namespace": "", "isInstalledPersonalApp": false, "developerName": "pep_web_app_2" }, "organization": { "organizationId": "00DgL000003KIqHUAW", "name": "Microsoft", "multicurrencyEnabled": false, "namespacePrefix": null, "currencyIsoCode": "USD" }, "environment": { "referer": null, "locationUrl": "https://orgfarm-1ee7d40ed5-dev-ed--c.develop.vf.force.com/apex/pep_web_app_2?sfdc.tabName=01rgL000004prBI&vfRetURLInSFX=%2Fhome%2Fhome.jsp&ltn_app_id=06mgL000001zOUvQAM&nonce=781140246aa70d20f7445a028ada117961d58edd350d758c5aa59e09f0c7b1a5&sfdcIFrameOrigin=https%3A%2F%2Forgfarm-1ee7d40ed5-dev-ed.develop.lightning.force.com&tour=&isdtp=p1&sfdcIFrameHost=web&clc=0", "displayLocation": "Visualforce", "sublocation": null, "uiTheme": "Theme3", "dimensions": { "width": "100%", "height": "1000px", "maxWidth": "1000px", "maxHeight": "2000px", "clientWidth": "1239px", "clientHeight": "30px" }, "parameters": {}, "record": {}, "version": { "season": "SPRING", "api": "63.0" } } } };
+      this.envelope = { 
+        "algorithm": "HMACSHA256", 
+        "issuedAt": -246115464, 
+        "userId": "005gL00000dasdas232iC2jQAE", 
+        "client": { 
+          "refreshToken": null, 
+          "instanceId": "_:pep_web_app_2:j_id0:j_id1:canvasapp", 
+          "targetOrigin": "https://orgfarm-1ee7d40ed5-dev-ed.develop.my.salesforce.com", 
+          "instanceUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.my.salesforce.com", 
+          "oauthToken": "dasdasdasdassoyuntokenadsasdasda" 
+        }, 
+        "context": { 
+          "user": { 
+            "userId": "005gL000002iC2jQAE", 
+            "userName": "dadsasda04@agentforce.com", 
+            "firstName": "adsdasdasdas", 
+            "lastName": "Mejia", 
+            "email": "adsasdasdasdas@adasdasdasd.com", 
+            "fullName": "Demo User", 
+            "locale": "es_PE", 
+            "language": "en_US", 
+            "timeZone": "America/Lima", 
+            "profileId": "00egL000001efNI", 
+            "roleId": null, 
+            "userType": "STANDARD", 
+            "currencyISOCode": "USD", 
+            "profilePhotoUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.file.force.com/profilephoto/005/F", 
+            "profileThumbnailUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.file.force.com/profilephoto/005/T", 
+            "siteUrl": null, 
+            "siteUrlPrefix": null, 
+            "networkId": null, 
+            "accessibilityModeEnabled": false, 
+            "isDefaultNetwork": true 
+          }, 
+          "links": { 
+            "loginUrl": "https://orgfarm-1ee7d40ed5-dev-ed.develop.my.salesforce.com", 
+            "enterpriseUrl": "/services/Soap/c/63.0/00DgL000003KIqH", 
+            "metadataUrl": "/services/Soap/m/63.0/00DgL000003KIqH", 
+            "partnerUrl": "/services/Soap/u/63.0/00DgL000003KIqH", 
+            "restUrl": "/services/data/v63.0/", 
+            "sobjectUrl": "/services/data/v63.0/sobjects/", 
+            "searchUrl": "/services/data/v63.0/search/", 
+            "queryUrl": "/services/data/v63.0/query/", 
+            "recentItemsUrl": "/services/data/v63.0/recent/", 
+            "chatterFeedsUrl": "/services/data/v31.0/chatter/feeds", 
+            "chatterGroupsUrl": "/services/data/v63.0/chatter/groups", 
+            "chatterUsersUrl": "/services/data/v63.0/chatter/users", 
+            "chatterFeedItemsUrl": "/services/data/v31.0/chatter/feed-items", 
+            "userUrl": "/005gL000002iC2jQAE" 
+          }, 
+          "application": { 
+            "name": "pep_web_app_2", 
+            "canvasUrl": "https://sfcanvaswebapp002.azurewebsites.net", 
+            "applicationId": "06PgL0000004hCr", 
+            "version": "1.0", 
+            "authType": "SIGNED_REQUEST", 
+            "referenceId": "09HgL0000004D3V", 
+            "options": [], 
+            "samlInitiationMethod": "None", 
+            "namespace": "", 
+            "isInstalledPersonalApp": false, 
+            "developerName": "pep_web_app_2" 
+          }, 
+          "organization": { 
+            "organizationId": "00DgL000003KIqHUAW", 
+            "name": "Salesforce Organization", 
+            "multicurrencyEnabled": false, 
+            "namespacePrefix": null, 
+            "currencyIsoCode": "USD" 
+          }, 
+          "environment": { 
+            "referer": null, 
+            "locationUrl": "https://orgfarm-1ee7d40ed5-dev-ed--c.develop.vf.force.com/apex/pep_web_app_2?sfdc.tabName=01rgL000004prBI&vfRetURLInSFX=%2Fhome%2Fhome.jsp&ltn_app_id=06mgL000001zOUvQAM&nonce=781140246aa70d20f7445a028ada117961d58edd350d758c5aa59e09f0c7b1a5&sfdcIFrameOrigin=https%3A%2F%2Forgfarm-1ee7d40ed5-dev-ed.develop.lightning.force.com&tour=&isdtp=p1&sfdcIFrameHost=web&clc=0", 
+            "displayLocation": "Visualforce", 
+            "sublocation": null, 
+            "uiTheme": "Theme3", 
+            "dimensions": { 
+              "width": "100%", 
+              "height": "1000px", 
+              "maxWidth": "1000px", 
+              "maxHeight": "2000px", 
+              "clientWidth": "1239px", 
+              "clientHeight": "30px" 
+            }, 
+            "parameters": {}, 
+            "record": {}, 
+            "version": { 
+              "season": "SPRING", 
+              "api": "63.0" 
+            } 
+          } 
+        } 
+      };
 
       // Show toast after a delay to simulate loading
       setTimeout(() => {
@@ -68,8 +173,7 @@ export class AppComponent implements OnInit {
       }, 500);
       setTimeout(() => {
         this.showToast = false;
-      }
-      , 3000);
+      }, 3000);
     }
   }
 
@@ -81,11 +185,34 @@ export class AppComponent implements OnInit {
   }
 
   /**
+   * Validates the API endpoint before making an API call
+   * @returns Whether the API endpoint is valid
+   */
+  validateApiEndpoint(): boolean {
+    if (!this.apiEndpoint) {
+      this.inputError = 'API endpoint is required';
+      return false;
+    }
+    
+    if (!this.apiEndpoint.startsWith('/services/data/')) {
+      this.inputError = 'API endpoint must start with /services/data/';
+      return false;
+    }
+    
+    this.inputError = null;
+    return true;
+  }
+
+  /**
    * Makes an API call to the Salesforce REST API
    */
   async testApiCall() {
     if (!this.envelope?.client?.oauthToken || !this.envelope?.client?.targetOrigin) {
       alert('No OAuth token or target origin available');
+      return;
+    }
+    
+    if (!this.validateApiEndpoint()) {
       return;
     }
 
